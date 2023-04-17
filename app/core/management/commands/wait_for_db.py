8f32e99fbc6a6d35edbf6f01e5c82bb741109161
NuraminPramono <pram19.info@gmal.com>
2023-04-17 21:53:50 +0700

    lesson 41 add wait for db fied

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(database=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write(
